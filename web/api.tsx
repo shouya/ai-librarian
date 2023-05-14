19b5ee7cf44431f57e5376f221d20e6dcb4d2cc4
Shou Ya <shouya@users.noreply.github.com>
2023-05-14 15:24:55 +0900

    format the code with 2-space indentation

--- web/api.tsx
+++ web/api.tsx
@@ -1,40 +1,50 @@
 export const listBooks = (callback) => {
-    return fetch("/api/books")
-        .then(response => response.json())
-        .then(json => json.map(book => {
-            return { id: book.book_id, title: book.name }
-        }))
-        .then(json => callback(json))
-        .catch(error => console.log(error));
+  return fetch("/api/books")
+    .then(response => response.json())
+    .then(json => json.map(book => {
+      return { id: book.book_id, title: book.name }
+    }))
+    .then(json => callback(json))
+    .catch(error => console.log(error));
 };
 
 export const listHistory = (book_id, callback) => {
-    return fetch(`/api/books/${book_id}/history`)
-        .then(response => response.json())
-        .then(json => json.map(entry => {
-            return {
-                ...entry,
-                references: entry.rel_docs || [],
-                id: entry.log_id
-            }
-        }))
-        .then(json => callback(json))
-        .catch(error => console.log(error));
+  if (!book_id) {
+    console.log(book_id);
+    return callback([]);
+  }
+
+  return fetch(`/api/books/${book_id}/history`)
+    .then(response => response.json())
+    .then(json => json.map(entry => {
+      return {
+        ...entry,
+        references: entry.rel_docs || [],
+        id: entry.log_id
+      }
+    }))
+    .then(json => callback(json))
+    .catch(error => console.log(error));
 };
 
 export const ask = (book_id: string, question: string, callback) => {
-    return fetch(`/api/books/${book_id}/ask?q=${encodeURIComponent(question)}`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        }
+  if (!book_id) {
+    console.log(book_id);
+    return callback(null);
+  }
+
+  return fetch(`/api/books/${book_id}/ask?q=${encodeURIComponent(question)}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
     })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("Network response was not ok");
-            }
-            return response.json();
-        })
-        .then(json => callback(json))
-        .catch(error => console.log(error));
+    .then(json => callback(json))
+    .catch(error => console.log(error));
 };
--- web/script.tsx
+++ web/script.tsx
@@ -4,157 +4,157 @@ import { createRoot } from "react-dom/client";
 import { listBooks, listHistory, ask } from "./api";
 
 function chatHistoryReducer(chatHistory, action) {
-    const { bookId } = action;
-    const history = chatHistory[bookId] || [];
+  const { bookId } = action;
+  const history = chatHistory[bookId] || [];
 
-    if (action.type == "add") {
-        const { question, answer, quote, log_id, rel_docs } = action;
-        const historyEntry = {
-            question, answer, quote, id: log_id, references: rel_docs
-        };
-        return { ...chatHistory, [bookId]: [historyEntry, ...history] }
-    } else if (action.type == "init") {
-        return { ...chatHistory, [bookId]: action.history }
-    } else {
-        throw new Error("Invalid action type");
-    }
+  if (action.type == "add") {
+    const { question, answer, quote, log_id, rel_docs } = action;
+    const historyEntry = {
+      question, answer, quote, id: log_id, references: rel_docs
+    };
+    return { ...chatHistory, [bookId]: [historyEntry, ...history] }
+  } else if (action.type == "init") {
+    return { ...chatHistory, [bookId]: action.history }
+  } else {
+    throw new Error("Invalid action type");
+  }
 }
 
 function BookListItem({ book, isCurrent, setCurrentBookId }) {
-    return <div
-        className={"book-list-item " + (isCurrent ? "current" : "")}
-        onClick={() => setCurrentBookId(book.id)}>
-        {book.title}
-    </div>
+  return <div
+    className={"book-list-item " + (isCurrent ? "current" : "")}
+    onClick={() => setCurrentBookId(book.id)}>
+    {book.title}
+  </div>
 }
 
 function BookList({ bookList, currentBookId, setCurrentBookId }) {
-    return <>
-        <div className="book-list">
-            <h2 class="heading" >Select a book</h2>
-            {
-                bookList.map(book =>
-                    <BookListItem
-                        key={book.id}
-                        book={book}
-                        isCurrent={book.id === currentBookId}
-                        setCurrentBookId={setCurrentBookId}
-                    />
-                )
-            }
-        </div>
-    </>;
+  return <>
+    <div className="book-list">
+      <h2 className="heading" >Select a book</h2>
+      {
+        bookList.map(book =>
+          <BookListItem
+            key={book.id}
+            book={book}
+            isCurrent={book.id === currentBookId}
+            setCurrentBookId={setCurrentBookId}
+          />
+        )
+      }
+    </div>
+  </>;
 }
 
 function Reference({ reference }) {
-    return <div className="reference">
-        <div className="content">{reference.content}</div>
-        <div className="metadata">
-            <div className="chapter">{reference.metadata.chapter_title}</div>
-            <div className="ref-id">{reference.id}</div>
-        </div>
-    </div>;
+  return <div className="reference">
+    <div className="content">{reference.content}</div>
+    <div className="metadata">
+      <div className="chapter">{reference.metadata.chapter_title}</div>
+      <div className="ref-id">{reference.id}</div>
+    </div>
+  </div>;
 }
 
 function ChatHistoryEntry({ history }) {
-    const [expand, setExpand] = useState(false);
-    const { question, answer, quote } = history;
-    return <div className="chat-history-entry">
-        <div className="question">{question}</div>
-        <div className="answer">{answer}</div>
-        <div className="quote" onClick={() => setExpand(e => !e)} title="Click to expand">{quote}</div>
-        <div className={"references " + (expand && "expanded")} >
-            {history.references.map(r => <Reference key={r.id} reference={r} />)}
-        </div>
-    </div>;
+  const [expand, setExpand] = useState(false);
+  const { question, answer, quote } = history;
+  return <div className="chat-history-entry">
+    <div className="question">{question}</div>
+    <div className="answer">{answer}</div>
+    <div className="quote" onClick={() => setExpand(e => !e)} title="Click to expand">{quote}</div>
+    <div className={"references " + (expand && "expanded")} >
+      {history.references.map(r => <Reference key={r.id} reference={r} />)}
+    </div>
+  </div>;
 }
 
 function ChatHistoryBacklog({ chatHistory }) {
-    return <div className="chat-history-backlog">
-        {chatHistory.map(entry =>
-            <ChatHistoryEntry key={entry.id} history={entry} />
-        )}
-    </div>;
+  return <div className="chat-history-backlog">
+    {chatHistory.map(entry =>
+      <ChatHistoryEntry key={entry.id} history={entry} />
+    )}
+  </div>;
 }
 
 function askQuestion(bookId, question, dispatch) {
-    if (question == "") return;
-    ask(bookId, question, resp => {
-        const { log_id, answer, quote, rel_docs } = resp;
-        dispatch({
-            type: "add",
-            bookId,
-            question,
-            answer,
-            quote,
-            log_id,
-            rel_docs
-        });
+  if (question == "") return;
+  ask(bookId, question, resp => {
+    const { log_id, answer, quote, rel_docs } = resp;
+    dispatch({
+      type: "add",
+      bookId,
+      question,
+      answer,
+      quote,
+      log_id,
+      rel_docs
     });
+  });
 }
 
 function AskBar({ bookId, dispatchChatHistory }) {
-    const input_ref = useRef(null);
-    const onSubmit = (e: InputEvent) => {
-        e.preventDefault();
-        const question = input_ref.current.value
-        askQuestion(bookId, question, dispatchChatHistory);
-        input_ref.current.value = "";
-    };
+  const input_ref = useRef(null);
+  const onSubmit = (e: InputEvent) => {
+    e.preventDefault();
+    const question = input_ref.current.value
+    askQuestion(bookId, question, dispatchChatHistory);
+    input_ref.current.value = "";
+  };
 
-    return <form className="ask-bar" onSubmit={onSubmit}>
-        <input type="text" ref={input_ref} />
-        <button>Ask</button>
-    </form>;
+  return <form className="ask-bar" onSubmit={onSubmit}>
+    <input type="text" ref={input_ref} />
+    <button>Ask</button>
+  </form>;
 }
 
 function ChatWindow({ bookId, book, chatHistory, dispatchChatHistory }) {
-    return <div className="chat-window">
-        <ChatHistoryBacklog chatHistory={chatHistory} />
-        <AskBar bookId={bookId} dispatchChatHistory={dispatchChatHistory} />
-    </div>
+  return <div className="chat-window">
+    <ChatHistoryBacklog chatHistory={chatHistory} />
+    <AskBar bookId={bookId} dispatchChatHistory={dispatchChatHistory} />
+  </div>
 }
 
 function App() {
-    const [bookList, setBookList] = useState([]);
-    const [currentBookId, setCurrentBookId] = useState(null);
-    const [chatHistory, dispatchChatHistory] = useReducer(chatHistoryReducer, []);
+  const [bookList, setBookList] = useState([]);
+  const [currentBookId, setCurrentBookId] = useState(null);
+  const [chatHistory, dispatchChatHistory] = useReducer(chatHistoryReducer, []);
 
-    useEffect(() => {
-        listBooks(books => {
-            setCurrentBookId(books[0]?.id);
-            setBookList(books);
-        });
-    }, []);
+  useEffect(() => {
+    listBooks(books => {
+      setCurrentBookId(books[0]?.id);
+      setBookList(books);
+    });
+  }, []);
 
-    useEffect(() => {
-        listHistory(currentBookId, history => {
-            dispatchChatHistory({ type: "init", bookId: currentBookId, history });
-        });
-    }, [currentBookId]);
+  useEffect(() => {
+    listHistory(currentBookId, history => {
+      dispatchChatHistory({ type: "init", bookId: currentBookId, history });
+    });
+  }, [currentBookId]);
 
 
-    const currentBook = bookList.find(book => book.id == currentBookId);
-    const currentHistory = currentBookId && chatHistory[currentBookId] || [];
+  const currentBook = bookList.find(book => book.id == currentBookId);
+  const currentHistory = currentBookId && chatHistory[currentBookId] || [];
 
-    return <div className="container">
-        <BookList
-            bookList={bookList}
-            currentBookId={currentBookId}
-            setCurrentBookId={setCurrentBookId}
-        />
-        <ChatWindow
-            bookId={currentBookId}
-            book={currentBook}
-            chatHistory={currentHistory}
-            dispatchChatHistory={dispatchChatHistory}
-        />
-    </div>;
+  return <div className="container">
+    <BookList
+      bookList={bookList}
+      currentBookId={currentBookId}
+      setCurrentBookId={setCurrentBookId}
+    />
+    <ChatWindow
+      bookId={currentBookId}
+      book={currentBook}
+      chatHistory={currentHistory}
+      dispatchChatHistory={dispatchChatHistory}
+    />
+  </div>;
 }
 
 const root = createRoot(document.getElementById("root"));
 root.render(
-    <StrictMode>
-        <App />
-    </StrictMode>
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
